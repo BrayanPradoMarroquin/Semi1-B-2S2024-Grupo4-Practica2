93d656c7a8f94a65505293d3a20ce13d26b0bb75
Pablo Jose Oliva Bonilla <Pabloolivaa@gmail.com>
2024-09-21 23:06:21 -0600

    [update] Se actualizo el frontend

--- photo_bucket/src/Components/Login/Login.js
+++ photo_bucket/src/Components/Login/Login.js
@@ -1,51 +1,53 @@
 // Login.js
-import React, { useState, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { InputText } from 'primereact/inputtext';
-import { Password } from 'primereact/password';
-import { Button } from 'primereact/button';
-import './Login.css';
+import React, { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { InputText } from "primereact/inputtext";
+import { Password } from "primereact/password";
+import { Button } from "primereact/button";
+import "./Login.css";
 
 function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [isCameraOn, setIsCameraOn] = useState(false);
   const videoRef = useRef(null);
   const navigate = useNavigate();
 
   const handleRegister = () => {
-    navigate('/register');
+    navigate("/register");
   };
 
   const handleLogin = async () => {
     if (!username || !password) {
-      alert('Por favor, complete todos los campos');
+      alert("Por favor, complete todos los campos");
       return;
     }
-    
+
     const loginData = {
       nombre_usuario: username,
-      contrasena: password
+      contrasena: password,
     };
 
     try {
-      const response = await fetch('http://localhost:8000/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(loginData),
-      });
+      const response = await fetch(
+        "http://balanceadorpractica2-564007876.us-east-1.elb.amazonaws.com/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(loginData),
+        }
+      );
 
       if (response.ok) {
-        alert('Inicio de sesión exitoso');
+        alert("Inicio de sesión exitoso");
       } else {
-        alert('Error en el inicio de sesión');
+        alert("Error en el inicio de sesión");
       }
     } catch (error) {
-      alert('Error en la petición: ' + error);
+      alert("Error en la petición: " + error);
     }
-
   };
 
   const handleFaceRecognition = async () => {
@@ -66,22 +68,45 @@ function Login() {
           <h2>Iniciar Sesión</h2>
           <div className="p-field">
             <label htmlFor="username">Nombre de Usuario</label>
-            <InputText id="username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Ingresa tu nombre de usuario" />
+            <InputText
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Ingresa tu nombre de usuario"
+            />
           </div>
           <div className="p-field">
             <label htmlFor="password">Contraseña</label>
-            <br/>
-            <Password id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Ingresa tu contraseña" feedback={false} />
+            <br />
+            <Password
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Ingresa tu contraseña"
+              feedback={false}
+            />
           </div>
-          <Button label="Iniciar Sesión" className="p-button-success" onClick={handleLogin} />
-          <Button label="Registrarse" className="p-button-secondary register-btn" onClick={handleRegister} />
+          <Button
+            label="Iniciar Sesión"
+            className="p-button-success"
+            onClick={handleLogin}
+          />
+          <Button
+            label="Registrarse"
+            className="p-button-secondary register-btn"
+            onClick={handleRegister}
+          />
         </div>
 
         <div className="divider-vertical"></div>
 
         <div className="login-section">
           <h3>O inicia sesión con reconocimiento facial</h3>
-          <Button label="Iniciar con reconocimiento facial" className="p-button-info" onClick={handleFaceRecognition} />
+          <Button
+            label="Iniciar con reconocimiento facial"
+            className="p-button-info"
+            onClick={handleFaceRecognition}
+          />
           {isCameraOn && (
             <div className="camera-container">
               <video ref={videoRef} autoPlay className="camera-feed"></video>
